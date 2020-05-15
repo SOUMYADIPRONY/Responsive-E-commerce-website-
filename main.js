d19ca55bcd0ea96fc85641883970df878be170f7
Soumyadip Dutta <52254186+SOUMYADIPRONY@users.noreply.github.com>
2020-05-15 19:32:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+/** First Slider */
+
+$(document).ready(function(){
+    $('.slider-one')
+     .not('.slick-initialized')
+     .slick({
+     autoplay:true,
+     autoplaySpeed:3000,
+     dots: true,
+     prevArrow:".site-slider .slider-btn.prev",
+     nextArrow:".site-slider .slider-btn.next",
+    
+    });
+  });
+
+
+  /** Second Slider */
+
+$(document).ready(function(){
+  $('.slider-two')
+   .not('.slick-initialized')
+   .slick({
+   prevArrow:".site-slider-two .prev",
+   nextArrow:".site-slider-two .next",
+   slidesToShow:5,
+   slidesToScroll: 1,
+   autoplaySpeed:3000
+  
+  });
+});
+
+
+  
